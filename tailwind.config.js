d804d44af0fd2ea2e46c4376421f9923519b527d
Vinay Tiwari <109553738+vinaytz@users.noreply.github.com>
2025-07-13 02:40:58 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+// tailwind.config.ts
+
+import { type Config } from "tailwindcss";
+
+const config: Config = {
+  darkMode: ["class"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
